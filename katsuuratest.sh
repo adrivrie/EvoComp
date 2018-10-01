1a9c1986122a50c42bf601b8b5838abfb22fb6d0
Vansil <silvandeboer@gmail.com>
2018-10-01 12:11:13 +0200

    Removing files that went into directory

--- katsuuratest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-javac -cp contest.jar player64.java
-jar cmf MainClass.txt submission.jar player64.class
-java -jar testrun.jar -submission=player64 -evaluation=KatsuuraEvaluation -seed=42
