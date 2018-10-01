e4a9f6fc5de3e92e763dbc546d56dd2dcdb14614
Maarten Boon <m_l_boon@hotmail.com>
2018-10-01 17:38:45 +0200

    Added option and argumentation for collection all fitnesses

--- islandModel/Data.java
+++ islandModel/Data.java
@@ -7,12 +7,21 @@ import java.util.ArrayList;
  */
 public class Data {
 
+	/** NEw FIELD
+	# Argumentation for this over the seperate fields:
+	# PRO: With this batch we can calculate/find all measures we might want that
+	# make use of fitness and generation/population level.
+	# CON: It is a larger bulk of data that has to be passed over...
+	// at index i, gives the fitness of all m individuals in the population after generation i
+	public ArrayList<ArrayLost<Double>> populationFitness = new ArrayList<ArrayList<Double>>();
+	*/
 	// at index i, gives the best fitness of the population after generation i
 	public ArrayList<Double> bestFitness = new ArrayList<Double>();
-	/** NEW FIELD
+	/** NEW FIELD (in case we decide not to implement the populationFitness)
 	// at index i, gives the variance of fitness for the population after generation i
 	public ArrayList<Double> populationVariance = new ArrayList<Double>();
 	*/
+
 	@Override
 	public String toString() {
 		String str = "";
