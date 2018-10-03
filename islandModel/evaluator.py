dd3ae2c8a103a41a8de7b5c0ccb53d68a605edb4
ozzypossie <oskar.vanderwal@gmail.com>
2018-10-03 12:32:30 +0200

    MBF and STD of best fitness working

--- islandModel/evaluator.py
+++ islandModel/evaluator.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 ### WORK IN PROGRESS
 
-def getBestFitness(algorithm):
+def plotBestFitness(algorithm):
     fitness_cols = algorithm.filter(regex='fitness')
     algorithm['mean fitness'] = fitness_cols.mean(axis=1)
     print(fitness_cols)
@@ -13,12 +13,49 @@ def getBestFitness(algorithm):
     algorithm.plot(kind='line',x='generation',y='mean fitness')
     plt.show()
 
-dummy = pd.read_csv("dummy.csv")
-print(dummy.head())
-print(dummy.shape)
-print(dummy.iloc[:,1].mean())
+# Evaluates the algorithm for performance measures and speed, results are put in the metaData file
+def evaluateAlgorithm(algorithm_name, run):
+    global metaData
+    algorithm = pd.read_csv("./islandModel/evaluation_files/" + str(algorithm_name) + "_" + str(run) + ".csv")
+    print(algorithm)
+    metaData.loc[(metaData['Algorithm']==algorithm_name) & (metaData['run']==run),"best fitness"] = getBestFitness(algorithm)
 
-getBestFitness(dummy)
+# Gets the best fitness in the whole run and adds it to the meta data
+def getBestFitness(algorithm):
+    fitness_cols = algorithm.filter(regex='fitness')
+    return(fitness_cols.values.max())
+
+def getMeanBestFitness(metaData_list):
+    uniqueAlgorithms = metaData_list['Algorithm'].unique()
+    for x in uniqueAlgorithms:
+        metaData.loc[metaData['Algorithm']==x, "MBF"] = metaData.loc[metaData['Algorithm']==x, "best fitness"].mean()
+
+def getBestFitnessSTD(metaData_list):
+    uniqueAlgorithms = metaData_list['Algorithm'].unique()
+    for x in uniqueAlgorithms:
+        metaData.loc[metaData['Algorithm']==x, "best fitness std"] = metaData.loc[metaData['Algorithm']==x, "best fitness"].std()
+
+
+metaData = pd.read_csv("./islandModel/evaluation_files/metaData.csv")
+metaData["best fitness"] = 0
+print(metaData)
+
+for index, row in metaData.iterrows():
+    evaluateAlgorithm(row["Algorithm"], row["run"])
+
+getMeanBestFitness(metaData)
+getBestFitnessSTD(metaData)
+
+print(metaData)
+
+
+#dummy = pd.read_csv("./islandModel/evaluation_files/dummy.csv")
+#print(dummy.head())
+#print(dummy.shape)
+#print(dummy.iloc[:,1].mean())
+
+#getBestFitness(dummy)
+
 
 #ax = plt.gca()
 
