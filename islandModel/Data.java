a6dadd0a7fe85eef298e70325431cc137c6356c7
Maarten Boon <m_l_boon@hotmail.com>
2018-10-09 14:38:08 +0200

    added filecheck and file creation to writeIteration

--- islandModel/Data.java
+++ islandModel/Data.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.*;
+import java.io.IOException;
 /**
- * 
+ *
  * This class contains all information that Evaluator needs of one run of player64.
  *
  */
@@ -9,58 +13,58 @@ public class Data {
 
 	// at index i, gives the best fitness of the population after generation i
 	public ArrayList<Double> bestFitness = new ArrayList<Double>();
-	
-	// full history of the run: 
-	// 	islands : 
+
+	// full history of the run:
+	// 	islands :
 	//		epoch : generation
 	//			individuals
 	//			parameters
 	public IslandHistory history;
-	
+
 	public Data(int nIslands) {
 		history = new IslandHistory(nIslands);
 	}
-	
-	
-	
+
+
+
 	// adds current islands to the island history
 	public void addIslandHistory(Island[] islands, int epochIndex) {
 		for (int islandIndex=0; islandIndex<islands.length; islandIndex++) {
 			history.addIsland(islands[islandIndex], islandIndex, epochIndex);
 		}
 	}
-	
-	
+
+
 	@Override
 	public String toString() {
 		String str = "";
 		str += String.format("dims: %d, %d, %d", history.epochHistory.size(),
 				history.epochHistory.get(0).size(),
 				history.epochHistory.get(0).get(0).size());
-		
-		
+
+
 //		str += "Best fitness per generation:\n\t[";
 //		for (double fitness : bestFitness) {
 //			str += String.format("%.1e, ", fitness);
 //		}
 //		str += "]";
-		
+
 		return str;
 	}
-	
+
 	// full history of the islands in one run
 	public class IslandHistory {
 		//  epochInd, islandInd, iterationInd -> island
 		public ArrayList<ArrayList<ArrayList<Island>>> epochHistory;
 		// number of islands
 		private int nIslands;
-		
+
 		public IslandHistory(int nIslands) {
 			this.nIslands = nIslands;
 			epochHistory = new ArrayList<ArrayList<ArrayList<Island>>>();
 		}
-		
-		
+
+
 		// returns a reference to the island with given index as it was in a given epoch in a given iteration
 		public Island getIsland(int epochIndex, int islandIndex, int generationIndex) {
 			if (epochHistory.size() > epochIndex) {
@@ -75,7 +79,7 @@ public class Data {
 			}
 			return null;
 		}
-		
+
 		// adds an island to the history at a certain epoch (if the island has already been added with the same generation number, it is assumed that it hasn't changed)
 		public void addIsland(Island island, int islandIndex, int epochIndex) {
 			if (epochHistory.size() == epochIndex) { // new epoch, add element to list
@@ -93,19 +97,52 @@ public class Data {
 				}
 			}
 		}
-		
-		
+
+
 	}
-	
+
 	public static void writeIteration(Island[] islands, int epochIndex) {
+		// Find correct path or create if it doesn't exist
+		Path currentPath = Paths.get(System.getProperty("user.dir"));
+		//System.out.println(currentPath.toString());
+		Path filePath = Paths.get(currentPath.toString(), "evaluation_files","test.csv");
+
+		if (Files.notExists(filePath.getParent())){
+			// Folder doesn't exist
+			System.out.println(filePath.getParent().toString()+" being created");
+			// Create folder and file
+			try {
+				Files.createDirectories(filePath.getParent());
+				Files.createFile(filePath);
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		} else if (Files.notExists(filePath)) {
+			System.out.println(filePath.toString()+" being created");
+			// Create file
+			try{
+				Files.createFile(filePath);
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		}
 		// write to csv
-		
-		for (Island island : islands) {
-			System.out.print(String.format("N=%d b=%.0e\t", island.population.size(), island.bestFitness));
+		try{
+			File file = new File(filePath.toString());
+			FileWriter writer = new FileWriter(file, true);
+			for (Island island : islands) {
+				if (!island.hasConverged){
+					writer.write(String.format("N=%d b=%.0e\t", island.population.size(), island.bestFitness));
+					//System.out.print(String.format("N=%d b=%.0e\t", island.population.size(), island.bestFitness));
+				}
+			}
+			writer.close();
+		} catch (IOException e){
+			e.printStackTrace();
 		}
 		System.out.println("(epoch " + epochIndex + ")");
-		
+
 		// PAS OP: check of een eiland nog runt, of al geconverged is (dan hoef je geen data op te slaan want er is niks veranderd)
 	}
-	
+
 }
