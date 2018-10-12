71f4a49330c3c15b760820bbb740942f61695454
Vansil <silvandeboer@gmail.com>
2018-10-12 14:01:39 +0200

    Delete old directories without any use

--- islandModelOldButNewer/compileAndRun.sh
+++
@@ -1,6 +0,0 @@
-#DOE DIT EERST IN DE TERMINAL: export LD_LIBRARY_PATH=~/path/to/this/dir/
-#EN DIT: cd ~/path/to/this/dir
-#NU KUN JE DIT RUNNEN
-javac -cp testrun.jar:submission.jar:contest.jar player64.java Evaluator.java Data.java Chromosome.java Island.java
-java -cp testrun.jar:submission.jar:contest.jar Evaluator
-
